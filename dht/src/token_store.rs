2689169d916a1d9eb1068740b43c5ce6776de3d4
Nehliin <oskar.d.nehlin@gmail.com>
2023-01-29 12:55:43 +0100

    Auto purge stale tokens and make token store shallow clonable

--- dht/src/token_store.rs
+++ dht/src/token_store.rs
@@ -15,12 +15,13 @@ const SECRET_REFRESH_TIME: Duration = Duration::from_secs(60 * 5);
 #[cfg(test)]
 const SECRET_REFRESH_TIME: Duration = Duration::from_secs(3);
 
+#[derive(Clone)]
 pub(crate) struct TokenStore {
     // Random secret that changes every SECRET_REFRESH_TIME
     // used to generate tokens
     current_secret: Rc<RefCell<[u8; 16]>>,
     prev_secret: Rc<RefCell<[u8; 16]>>,
-    received_tokens: AHashMap<Ipv4Addr, (Instant, Bytes)>,
+    received_tokens: Rc<RefCell<AHashMap<Ipv4Addr, (Instant, Bytes)>>>,
     shutdown: Rc<RefCell<bool>>,
 }
 
@@ -83,19 +84,21 @@ impl TokenStore {
     }
 
     #[inline]
-    pub fn get_token(&self, ip: Ipv4Addr) -> Option<&Bytes> {
-        // TODO: Remove the tokens as well at some point
-        self.received_tokens
-            .get(&ip)
-            .filter(|(inserted_at, _token)| {
-                Instant::now() - *inserted_at <= (SECRET_REFRESH_TIME * 2)
-            })
-            .map(|(_, token)| token)
+    pub fn get_token(&self, ip: Ipv4Addr) -> Option<Bytes> {
+        let mut received_tokens = self.received_tokens.borrow_mut();
+        let (inserted_at, token) = received_tokens.remove(&ip)?;
+        if Instant::now() - inserted_at <= (SECRET_REFRESH_TIME * 2) {
+            received_tokens.insert(ip, (inserted_at, token.clone()));
+            Some(token)
+        } else {
+            None
+        }
     }
 
     #[inline]
-    pub fn store_token(&mut self, ip: Ipv4Addr, token: Bytes) {
-        self.received_tokens.insert(ip, (Instant::now(), token));
+    pub fn store_token(&self, ip: Ipv4Addr, token: Bytes) {
+        let mut received_tokens = self.received_tokens.borrow_mut();
+        received_tokens.insert(ip, (Instant::now(), token));
     }
 }
 
@@ -144,11 +147,12 @@ mod test {
     #[test]
     fn does_not_store_tokens_forever() {
         tokio_uring::start(async {
-            let mut token_store = TokenStore::new();
+            let token_store = TokenStore::new();
             let ip = Ipv4Addr::new(127, 3, 4, 5);
             let generated = token_store.generate(ip);
             token_store.store_token(ip, generated);
             tokio::time::sleep(SECRET_REFRESH_TIME).await;
+            assert!(token_store.get_token(ip).is_some());
             assert!(token_store.get_token(ip).is_some());
             tokio::time::sleep(SECRET_REFRESH_TIME + Duration::from_secs(1)).await;
             assert!(token_store.get_token(ip).is_none());
