41ddaa172f9336b93646e61ba482e4d5a05ef7a5
Nehliin <oskar.d.nehlin@gmail.com>
2023-03-05 14:53:57 +0100

    Make filestore accept AsRef<Path>

--- bittorrent/src/drive_io.rs
+++ bittorrent/src/drive_io.rs
@@ -1,10 +1,5 @@
-use std::{
-    path::{Path, PathBuf},
-    rc::Rc,
-};
+use std::path::{Path, PathBuf};
 
-use anyhow::Context;
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio_uring::{
     buf::BoundedBuf,
     fs::{File, OpenOptions},
@@ -32,57 +27,62 @@ pub struct FileStore {
 }
 
 impl FileStore {
-    // TODO take AsRef<Path> instead
-    pub async fn new(root: &Path, torrent_info: &bip_metainfo::Info) -> anyhow::Result<Self> {
-        let mut result = Vec::new();
-        let mut start_piece = 0;
-        let mut start_offset = 0;
+    pub async fn new(
+        root: impl AsRef<Path>,
+        torrent_info: &bip_metainfo::Info,
+    ) -> anyhow::Result<Self> {
+        async fn new_impl(root: &Path, torrent_info: &bip_metainfo::Info) -> anyhow::Result<FileStore> {
+            let mut result = Vec::new();
+            let mut start_piece = 0;
+            let mut start_offset = 0;
 
-        let piece_length = torrent_info.piece_length();
+            let piece_length = torrent_info.piece_length();
 
-        for torrent_file in torrent_info.files() {
-            let num_pieces = (torrent_file.length() + start_offset as u64) / piece_length;
-            let offset = (torrent_file.length() + start_offset as u64) % piece_length;
+            for torrent_file in torrent_info.files() {
+                let num_pieces = (torrent_file.length() + start_offset as u64) / piece_length;
+                let offset = (torrent_file.length() + start_offset as u64) % piece_length;
 
-            let mut path_buf = PathBuf::from(root);
-            path_buf.push(torrent_file.path());
-            if let Some(parent_dir) = path_buf.parent() {
-                // TODO: consider caching already created directories
-                tokio_uring::fs::create_dir_all(parent_dir).await?;
+                let mut path_buf = PathBuf::from(root);
+                path_buf.push(torrent_file.path());
+                if let Some(parent_dir) = path_buf.parent() {
+                    // TODO: consider caching already created directories
+                    tokio_uring::fs::create_dir_all(parent_dir).await?;
+                }
+                let file = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create(true)
+                    .open(&path_buf)
+                    .await?;
+
+                // Default mode is described in the man pages as:
+                // allocates the disk space within the range specified by offset and
+                // len. The file size (as reported by stat(2)) will be changed if
+                // offset+len is greater than the file size.  Any subregion within
+                // the range specified by offset and len that did not contain data
+                // before the call will be initialized to zero.
+                const DEFAULT_FALLOCATE_MODE: i32 = 0;
+
+                file.fallocate(0, torrent_file.length(), DEFAULT_FALLOCATE_MODE)
+                    .await?;
+
+                let torrent_file = TorrentFile {
+                    start_piece,
+                    start_offset,
+                    end_piece: start_piece + num_pieces as i32,
+                    end_offset: offset as i32,
+                    file,
+                };
+                start_piece = torrent_file.end_piece;
+                start_offset = torrent_file.end_offset;
+                result.push(torrent_file)
             }
-            let file = OpenOptions::new()
-                .read(true)
-                .write(true)
-                .create(true)
-                .open(&path_buf)
-                .await?;
-
-            // Default mode is described in the man pages as:
-            // allocates the disk space within the range specified by offset and
-            // len. The file size (as reported by stat(2)) will be changed if
-            // offset+len is greater than the file size.  Any subregion within
-            // the range specified by offset and len that did not contain data
-            // before the call will be initialized to zero.
-            const DEFAULT_FALLOCATE_MODE: i32 = 0;
-
-            file.fallocate(0, torrent_file.length(), DEFAULT_FALLOCATE_MODE)
-                .await?;
-
-            let torrent_file = TorrentFile {
-                start_piece,
-                start_offset,
-                end_piece: start_piece + num_pieces as i32,
-                end_offset: offset as i32,
-                file,
-            };
-            start_piece = torrent_file.end_piece;
-            start_offset = torrent_file.end_offset;
-            result.push(torrent_file)
+            Ok(FileStore {
+                files: result,
+                piece_length,
+            })
         }
-        Ok(FileStore {
-            files: result,
-            piece_length,
-        })
+        new_impl(root.as_ref(), torrent_info).await
     }
 
     pub async fn write_piece(&self, index: i32, mut piece_data: Vec<u8>) -> anyhow::Result<()> {
@@ -297,7 +297,7 @@ mod tests {
 
         let mut data_clone = data.clone();
         tokio_uring::start(async move {
-            let file_store = FileStore::new(Path::new(root_dir), torrent_info.info())
+            let file_store = FileStore::new(root_dir, torrent_info.info())
                 .await
                 .unwrap();
 
@@ -340,7 +340,7 @@ mod tests {
 
         let mut data_clone = data.clone();
         tokio_uring::start(async move {
-            let file_store = FileStore::new(Path::new(root_dir), torrent_info.info())
+            let file_store = FileStore::new(root_dir, torrent_info.info())
                 .await
                 .unwrap();
 
