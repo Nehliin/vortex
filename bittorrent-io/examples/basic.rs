ccc844224a6200200bca90c397c2cb56e0c559a9
Nehliin <>
2024-11-03 19:22:12 +0100

    some refactors

--- bittorrent-io/examples/basic.rs
+++ bittorrent-io/examples/basic.rs
@@ -3,6 +3,10 @@ use std::{io::Write, net::TcpStream};
 use bittorrent_io::setup_listener;
 
 fn main() {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        //        .target(env_logger::Target::Pipe(Box::new(log_file)))
+        .init();
     let handle = std::thread::spawn(|| {
         setup_listener();
     });
@@ -15,6 +19,6 @@ fn main() {
     // 36 + 10 * 300
     for _ in 0..300 {
         stream.write_all(b"SOMETHING\n").unwrap();
-   }
+    }
     handle.join().unwrap();
 }
