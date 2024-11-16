c0162f6310d03ea7d925b18292ddcd8468ee6e72
Nehliin <>
2024-11-16 14:54:29 +0100

    Continue filling in peer connection

--- bittorrent-io/src/peer_connection.rs
+++ bittorrent-io/src/peer_connection.rs
@@ -1,17 +1,53 @@
+use core::str;
 use std::{
-    io::{self, ErrorKind},
+    fmt::Display,
+    io::{self},
     os::fd::RawFd,
 };
 
-use bytes::{Buf, BufMut};
+use bytes::Bytes;
+use thiserror::Error;
 
-use crate::peer_protocol::{PeerMessage, PeerMessageDecoder};
+use crate::{
+    peer_protocol::{PeerMessage, PeerMessageDecoder},
+    piece_selector::{Piece, Subpiece, SUBPIECE_SIZE},
+    TorrentState,
+};
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Peer should be disconnected")]
+    Disconnect,
+    #[error("Peer encountered IO issue")]
+    Io(#[source] io::Error),
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(transparent)]
+pub struct PeerId([u8; 20]);
+
+impl Display for PeerId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Ok(prefix) = str::from_utf8(&self.0[..8]) {
+            write!(f, "{prefix}")?;
+            for b in &self.0[8..] {
+                write!(f, "{:#x}", b)?;
+            }
+            Ok(())
+        } else {
+            for b in &self.0[..] {
+                write!(f, "{:#x}", b)?;
+            }
+            Ok(())
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct PeerConnection {
     pub fd: RawFd,
     // TODO: Make this a type that impl display
-    pub peer_id: [u8; 20],
+    pub peer_id: PeerId,
     /// This side is choking the peer
     pub is_choking: bool,
     /// This side is interested what the peer has to offer
@@ -21,10 +57,10 @@ pub struct PeerConnection {
     /// The peer is interested what we have to offer
     pub peer_interested: bool,
     // TODO: do we need this both with queued?
-    //pub currently_downloading: Vec<Piece>,
-    //pub queue_capacity: usize,
-    //pub queued: Vec<InflightSubpiece>,
-    //pub slow_start: bool,
+    pub currently_downloading: Vec<Piece>,
+    pub queue_capacity: usize,
+    pub queued: Vec<Subpiece>,
+    pub slow_start: bool,
     //pub moving_rtt: MovingRttAverage,
     // TODO calculate this meaningfully
     //pub througput: u64,
@@ -32,10 +68,12 @@ pub struct PeerConnection {
     // because of low througput. (Choke instead?)
     //pub pending_disconnect: bool,
     pub stateful_decoder: PeerMessageDecoder,
+    // Stored here to prevent reallocations
+    pub outgoing_msgs_buffer: Vec<PeerMessage>,
 }
 
 impl PeerConnection {
-    pub fn new(fd: RawFd, peer_id: [u8; 20]) -> Self {
+    pub fn new(fd: RawFd, peer_id: PeerId) -> Self {
         PeerConnection {
             fd,
             peer_id,
@@ -43,68 +81,265 @@ impl PeerConnection {
             is_interested: true,
             peer_choking: true,
             peer_interested: false,
+            queued: Vec::with_capacity(4),
+            queue_capacity: 4,
+            slow_start: true,
+            currently_downloading: Default::default(),
+            outgoing_msgs_buffer: Default::default(),
             stateful_decoder: PeerMessageDecoder::new(2 << 15),
         }
     }
 
-    pub fn handle_message(&mut self, peer_message: PeerMessage) -> io::Result<()> {
+    fn unchoke(&mut self, torrent_state: &mut TorrentState) {
+        if self.is_choking {
+            torrent_state.num_unchoked += 1;
+        }
+        self.is_choking = false;
+        self.outgoing_msgs_buffer.push(PeerMessage::Unchoke);
+    }
+
+    fn choke(&mut self, torrent_state: &mut TorrentState) {
+        if !self.is_choking {
+            torrent_state.num_unchoked -= 1;
+        }
+        self.is_choking = true;
+        self.outgoing_msgs_buffer.push(PeerMessage::Choke);
+    }
+
+    fn request_piece(&mut self, index: i32, length: u32) {
+        self.currently_downloading.push(Piece::new(index, length));
+        self.fill_request_queue()
+    }
+
+    pub fn fill_request_queue(&mut self) {
+        'outer: for piece in self.currently_downloading.iter_mut() {
+            let mut available_pieces = piece.completed_subpieces.clone();
+            available_pieces |= &piece.inflight_subpieces;
+            while let Some(subindex) = available_pieces.first_zero() {
+                if self.queued.len() < self.queue_capacity {
+                    piece.inflight_subpieces.set(subindex, true);
+                    // must update to prevent re-requesting same piece
+                    available_pieces.set(subindex, true);
+                    let length = if subindex as i32 == piece.last_subpiece_index() {
+                        piece.last_subpiece_length
+                    } else {
+                        SUBPIECE_SIZE
+                    };
+                    let subpiece_request = PeerMessage::Request {
+                        index: piece.index,
+                        begin: subindex as i32 * SUBPIECE_SIZE,
+                        length,
+                    };
+                    self.queued.push(Subpiece {
+                        index: piece.index,
+                        offset: subindex as i32 * SUBPIECE_SIZE,
+                        size: length,
+                    });
+                    self.outgoing_msgs_buffer.push(subpiece_request);
+                } else {
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    fn on_subpiece(&mut self, m_index: i32, m_begin: i32, data: Bytes) -> Option<Piece> {
+        let position = self
+            .currently_downloading
+            .iter()
+            .position(|piece| piece.index == m_index);
+        if let Some(position) = position {
+            let mut piece = self.currently_downloading.swap_remove(position);
+            piece.on_subpiece(m_index, m_begin, &data[..], self.peer_id);
+            if !piece.is_complete() {
+                // Next subpice to download (that isn't already inflight)
+                while let Some(next_subpice) = piece.next_unstarted_subpice() {
+                    if self.queued.len() < self.queue_capacity {
+                        piece.inflight_subpieces.set(next_subpice, true);
+                        let length = if next_subpice as i32 == piece.last_subpiece_index() {
+                            piece.last_subpiece_length
+                        } else {
+                            SUBPIECE_SIZE
+                        };
+                        let subpiece_request = PeerMessage::Request {
+                            index: piece.index,
+                            begin: SUBPIECE_SIZE * next_subpice as i32,
+                            length,
+                        };
+                        self.queued.push(Subpiece {
+                            size: length,
+                            index: piece.index,
+                            offset: SUBPIECE_SIZE * next_subpice as i32,
+                        });
+                        self.outgoing_msgs_buffer.push(subpiece_request);
+                    } else {
+                        break;
+                    }
+                }
+                // still downloading
+                self.currently_downloading.push(piece);
+            } else {
+                log::debug!("[Peer {}] Piece completed", self.peer_id);
+                return Some(piece);
+            }
+        } else {
+            log::error!(
+                "[Peer {}] Recieved unexpected piece message, index: {m_index}",
+                self.peer_id
+            );
+        }
+        None
+    }
+
+    pub fn handle_message(
+        &mut self,
+        conn_id: usize,
+        peer_message: PeerMessage,
+        torrent_state: &mut TorrentState,
+    ) -> Result<&[PeerMessage], Error> {
+        self.outgoing_msgs_buffer.clear();
         log::debug!("Received: {peer_message:?}");
-        /*match peer_message {
+        match peer_message {
             PeerMessage::Choke => {
-                log::info!("[Peer: {:?}] Peer is choking us!", self.peer_id);
+                log::info!("[Peer: {}] Peer is choking us!", self.peer_id);
                 self.peer_choking = true;
             }
             PeerMessage::Unchoke => {
                 self.peer_choking = false;
                 if !self.is_interested {
                     // Not interested so don't do anything
-                    return Ok(());
+                    return Ok(&self.outgoing_msgs_buffer);
                 }
-                // TODO: Get rid of this, should be allowed to continue here
-                if !peer_connection.state().currently_downloading.is_empty() {
-                    return Ok(());
+                // TODO: Get rid of this, should be allowed to continue here?
+                // or this is controlled by the tick?
+                if !self.currently_downloading.is_empty() {
+                    return Ok(&self.outgoing_msgs_buffer);
                 }
-                if let Some(piece_idx) = self.piece_selector.next_piece(peer_key) {
-                    if let Err(err) = peer_connection.unchoke() {
-                        log::error!("[PeerKey: {peer_key:?}] Peer disconnected: {err}");
-                        // TODO: cleaner fix here
-                        self.peer_list.connections.remove(peer_key);
-                        return Ok(());
-                    } else {
-                        self.num_unchoked += 1;
-                    }
-                    if let Err(err) = peer_connection
-                        .request_piece(piece_idx, self.piece_selector.piece_len(piece_idx))
-                    {
-                        log::error!("[PeerKey: {peer_key:?}] Peer disconnected: {err}");
-                        // TODO: cleaner fix here
-                        self.peer_list.connections.remove(peer_key);
-                        return Ok(());
-                    }
-                    self.piece_selector.mark_inflight(piece_idx as usize);
+                if let Some(piece_idx) = torrent_state.piece_selector.next_piece(conn_id) {
+                    self.unchoke(torrent_state);
+
+                    self.request_piece(
+                        piece_idx,
+                        torrent_state.piece_selector.piece_len(piece_idx),
+                    );
+                    torrent_state
+                        .piece_selector
+                        .mark_inflight(piece_idx as usize);
                 } else {
-                    log::warn!("[PeerKey: {peer_key:?}] No more pieces available");
+                    log::warn!("[Peer: {}] No more pieces available", self.peer_id);
                 }
             }
-            PeerMessage::Interested => todo!(),
-            PeerMessage::NotInterested => todo!(),
-            PeerMessage::Have { index } => todo!(),
-            PeerMessage::Bitfield(_) => todo!(),
+            PeerMessage::Interested => {
+                let should_unchoke = torrent_state.should_unchoke();
+                log::info!("[Peer: {}] Peer is interested in us!", self.peer_id);
+                self.peer_interested = true;
+                if !self.is_choking {
+                    // if we are not choking them we might need to send a
+                    // unchoke to avoid some race conditions. Libtorrent
+                    // uses the same type of logic
+                    self.unchoke(torrent_state);
+                } else if should_unchoke {
+                    log::debug!("[Peer: {}] Unchoking peer after intrest", self.peer_id);
+                    self.unchoke(torrent_state);
+                }
+            }
+            PeerMessage::NotInterested => {
+                self.peer_interested = false;
+                log::info!(
+                    "[Peer: {}] Peer is no longer interested in us!",
+                    self.peer_id
+                );
+                self.choke(torrent_state);
+            }
+            PeerMessage::Have { index } => {
+                log::info!(
+                    "[Peer: {}] Peer have piece with index: {index}",
+                    self.peer_id
+                );
+                torrent_state
+                    .piece_selector
+                    .set_peer_piece(conn_id, index as usize);
+            }
+            PeerMessage::Bitfield(mut field) => {
+                if torrent_state.torrent_info.pieces.len() != field.len() {
+                    if field.len() < torrent_state.torrent_info.pieces.len() {
+                        log::error!(
+                            "[Peer: {}] Received invalid bitfield, expected {}, got: {}",
+                            self.peer_id,
+                            torrent_state.torrent_info.pieces.len(),
+                            field.len()
+                        );
+                        return Err(Error::Disconnect);
+                    }
+                    // The bitfield might be padded with zeros, remove them first
+                    log::debug!(
+                        "[Peer: {}] Received padded bitfield, expected {}, got: {}",
+                        self.peer_id,
+                        torrent_state.torrent_info.pieces.len(),
+                        field.len()
+                    );
+                    field.truncate(torrent_state.torrent_info.pieces.len());
+                }
+                log::info!("[Peer: {}] Bifield received: {field}", self.peer_id);
+                torrent_state
+                    .piece_selector
+                    .update_peer_pieces(conn_id, field.into_boxed_bitslice());
+            }
             PeerMessage::Request {
                 index,
                 begin,
                 length,
-            } => todo!(),
+            } => {
+                let should_unchoke = torrent_state.should_unchoke();
+                if should_unchoke && self.is_choking {
+                    self.unchoke(torrent_state);
+                }
+                if !self.is_choking {
+                    unimplemented!();
+                    /*match self.on_piece_request(index, begin, length).await {
+                        Ok(piece_data) => {
+                            let peer_connection = connection_mut_or_return!();
+                            peer_connection.piece(index, begin, piece_data)?;
+                        }
+                        Err(err) => {
+                            log::error!("[Peer: {}] Invalid piece request: {err}", self.peer_id)
+                        }
+                    }*/
+                } else {
+                    log::info!(
+                        "[Peer: {}] Piece request ignored, peer can't be unchoked",
+                        self.peer_id
+                    );
+                }
+            }
             PeerMessage::Cancel {
                 index,
                 begin,
                 length,
             } => todo!(),
-            PeerMessage::Piece { index, begin, data } => todo!(),
-        }*/
-        Ok(())
+            PeerMessage::Piece { index, begin, data } => {
+                log::debug!(
+                    "[Peer: {}] Recived a piece index: {index}, begin: {begin}, length: {}",
+                    self.peer_id,
+                    data.len(),
+                );
+                //let connection_state = peer_connection.state_mut();
+                //connection_state.update_stats(index, begin, data.len() as u32);
+                if let Some(piece) = self.on_subpiece(index, begin, data) {
+                    log::info!(
+                        "[Peer: {}] Piece {}/{} completed!",
+                        self.peer_id,
+                        torrent_state.piece_selector.total_completed(),
+                        torrent_state.piece_selector.pieces()
+                    );
+                    //self.on_piece_completed(piece.index, piece.memory).await;
+                    torrent_state.piece_selector.mark_complete(piece.index as usize);
+                    if torrent_state.piece_selector.completed_all() {
+                        panic!("TOrrent complete!")
+                    }
+                }
+            }
+        }
+        Ok(&self.outgoing_msgs_buffer)
     }
 }
-
-// pub peer_id: [u8; 20],
-// pub peer_addr: SocketAddr,
