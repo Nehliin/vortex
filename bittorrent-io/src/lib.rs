bee1cefa70eb665185ce13e5dbe3def4195d9d98
Nehliin <>
2024-12-02 21:40:39 +0100

    Implement tick

--- bittorrent-io/src/lib.rs
+++ bittorrent-io/src/lib.rs
@@ -34,12 +34,12 @@ pub fn setup_listener(torrent_state: TorrentState) {
     let mut ring: IoUring = IoUring::builder()
         .setup_single_issuer()
         .setup_clamp()
-        .setup_cqsize(1024)
+        .setup_cqsize(4096)
         .setup_defer_taskrun()
-        .build(1024)
+        .build(4096)
         .unwrap();
 
-    let mut events = Slab::with_capacity(256);
+    let mut events = Slab::with_capacity(4096);
     let event_idx = events.insert(Event::Accept);
     let user_data = UserData::new(event_idx, None);
 
@@ -172,9 +172,92 @@ pub fn connect_to(addr: SocketAddr, torrent_state: TorrentState) {
 }
 
 // Validate hashes in here and simply use one shot channels
-fn tick(tick_delta: &Duration, connections: &mut Slab<PeerConnection>) {
+fn tick(
+    tick_delta: &Duration,
+    connections: &mut Slab<PeerConnection>,
+    piece_selector: &mut PieceSelector,
+) {
     log::info!("Tick!: {}", tick_delta.as_secs_f32());
     // 1. Calculate bandwidth (deal with initial start up)
     // 2. Go through them in order
     // 3. select pieces
+    let now = Instant::now();
+    // TODO use retain instead of iter mut in the loop below and get rid of this
+    //let mut disconnects = Vec::new();
+    for (id, connection) in connections.iter_mut() {
+        // If this connection have no inflight 2 iterations in a row
+        // disconnect it and clear all pieces it was currently downloading
+        // this is not very granular and will need tweaking
+
+        if connection.pending_disconnect && !connection.peer_choking && connection.queued.is_empty()
+        {
+            log::error!("Disconnect");
+            //disconnects.push(peer_key);
+            continue;
+        }
+
+        if !connection.peer_choking {
+            // slow start win size increase is handled in update_stats
+            if !connection.slow_start && !connection.pending_disconnect {
+                // calculate new bandwitdth_delay product and set request queues
+                let bandwitdth_delay =
+                    connection.moving_rtt.mean().as_millis() as u64 * connection.througput;
+                let new_queue_capacity = bandwitdth_delay / piece_selector::SUBPIECE_SIZE as u64;
+                connection.queue_capacity = new_queue_capacity as usize;
+            }
+            connection.queue_capacity = connection.queue_capacity.max(1);
+        }
+        log::info!(
+                        "[Peer {}]: throughput: {} bit/s, queue: {}/{}, rtt_mean: {}ms, currently_downloading: {}",
+                        connection.peer_id,
+                        connection.througput,
+                        connection.queued.len(),
+                        connection.queue_capacity,
+                        connection.moving_rtt.mean().as_millis(),
+                        connection.currently_downloading.len()
+                    );
+        connection.througput = 0;
+        // TODO: add to throughput total stats
+    }
+
+    // Request new pieces and fill up request queues
+    let mut peer_bandwidth: Vec<_> = connections
+        .iter_mut()
+        .map(|(key, peer)| (key, peer.remaining_request_queue_spots()))
+        .collect();
+
+    peer_bandwidth.sort_unstable_by(|(_, a), (_, b)| a.cmp(b).reverse());
+    for (peer_key, mut bandwidth) in peer_bandwidth {
+        let peer = &mut connections[peer_key];
+
+        while {
+            let bandwitdth_available_for_new_piece =
+                bandwidth > (piece_selector.avg_num_subpieces() as usize / 2);
+            let first_piece = peer.currently_downloading.is_empty() && !peer.peer_choking;
+            bandwitdth_available_for_new_piece || first_piece
+        } {
+            if let Some(next_piece) = piece_selector.next_piece(peer_key) {
+                if peer.is_choking {
+                    // TODO highly unclear if unchoke is desired here
+                    //if let Err(err) = peer.unchoke() {
+                    //   log::error!("{err}");
+                    //disconnects.push(peer_key);
+                    break;
+                    //} else {
+                    //   self.num_unchoked += 1;
+                    //}
+                }
+                peer.currently_downloading
+                    .push(Piece::new(next_piece, piece_selector.piece_len(next_piece)));
+                // Remove all subpieces from available bandwidth
+                bandwidth -= (piece_selector.num_subpieces(next_piece) as usize).min(bandwidth);
+                dbg!(bandwidth);
+                piece_selector.mark_inflight(next_piece as usize);
+            } else {
+                break;
+            }
+        }
+        peer.fill_request_queue();
+    }
+    
 }
