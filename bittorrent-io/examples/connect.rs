df379284bf28a9d80114ba136ef12d2dc194db5d
Nehliin <>
2024-12-05 22:23:51 +0100

    time download in example

--- bittorrent-io/examples/connect.rs
+++ bittorrent-io/examples/connect.rs
@@ -1,6 +1,7 @@
 use std::{
     io::Write,
     net::{TcpListener, TcpStream},
+    time::Instant,
 };
 
 use bittorrent_io::{connect_to, TorrentState};
@@ -21,6 +22,12 @@ fn main() {
     //});
     //std::net::TcpStream::connect("172.17.0.2:51413").unwrap();
 
+    let download_time = Instant::now();
     connect_to("172.17.0.2:51413".parse().unwrap(), torrent);
+    let elapsed = download_time.elapsed();
+    log::info!("Download complete in: {}s", elapsed.as_secs());
+    let expected = std::fs::read("../../bittorrent/assets/test-file-1").unwrap();
+    let actual = std::fs::read("../../bittorrent/dowloaded/test-file-1").unwrap();
+    assert_eq!(actual, expected);
     //handle.join().unwrap();
 }
