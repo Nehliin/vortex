924725dd84acdedf9eac337085a48148af3d86f1
Nehliin <>
2025-02-15 18:08:34 +0100

    Make example the first integration test

--- bittorrent/tests/basic.rs
+++ bittorrent/tests/basic.rs
@@ -2,23 +2,23 @@ use std::time::Instant;
 
 use vortex_bittorrent::{generate_peer_id, Torrent};
 
-fn main() {
+#[test]
+fn basic_seeded_download() {
     env_logger::builder()
         .filter_level(log::LevelFilter::Trace)
         .init();
     let torrent =
-        lava_torrent::torrent::v1::Torrent::read_from_file("bittorrent/assets/test-file-1.torrent")
-            .unwrap();
+        lava_torrent::torrent::v1::Torrent::read_from_file("../assets/test-file-1.torrent").unwrap();
     let our_id = generate_peer_id();
     let torrent = Torrent::new(torrent, our_id);
 
     let download_time = Instant::now();
     let (tx, rc) = std::sync::mpsc::channel();
-    tx.send("172.17.0.2:51413".parse().unwrap()).unwrap();
-    torrent.start(rc).unwrap();
+    tx.send("127.0.0.1:51413".parse().unwrap()).unwrap();
+    torrent.start(rc, "../downloaded").unwrap();
     let elapsed = download_time.elapsed();
     log::info!("Download complete in: {}s", elapsed.as_secs());
-    let expected = std::fs::read("bittorrent/assets/test-file-1").unwrap();
-    let actual = std::fs::read("bittorrent/downloaded/test-file-1/test-file-1").unwrap();
+    let expected = std::fs::read("../assets/test-file-1").unwrap();
+    let actual = std::fs::read("../downloaded/test-file-1/test-file-1").unwrap();
     assert_eq!(actual, expected);
 }
