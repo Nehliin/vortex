80f724183a3008898c303a0fb98568d11a914ea1
Nehliin <>
2025-06-07 12:31:43 +0200

    Add clap support in cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,23 +3,73 @@ use std::{
     time::{Duration, Instant},
 };
 
+use clap::{Args, Parser};
 use mainline::{Dht, Id};
 use metrics_exporter_prometheus::PrometheusBuilder;
-use vortex_bittorrent::{Command, Torrent, generate_peer_id};
+use vortex_bittorrent::{Command, State, Torrent, generate_peer_id};
+
+pub fn decode_info_hash_hex(s: &str) -> [u8; 20] {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16).unwrap())
+        .collect::<Vec<u8>>()
+        .try_into()
+        .unwrap()
+}
+
+#[derive(Debug, Args)]
+#[group(required = true, multiple = false)]
+struct TorrentInfo {
+    /// Info hash of the torrent you want to download.
+    /// The metadata will be automatically downloaded
+    /// in the swarm before download starts
+    #[arg(short, long)]
+    info_hash: Option<String>,
+    /// Torrent file containing the metadata of the torrent.
+    /// if this is provided the initial metadata download will
+    /// be skipped and the torrent downlaod can start immediately
+    #[arg(short, long)]
+    torrent_file: Option<PathBuf>,
+}
+
+/// Vortex bittorrent client cli. Fast trackerless torrent downloads using modern io_uring techniques.
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(flatten)]
+    torrent_info: TorrentInfo,
+    /// Path where the downloaded files should be saved
+    #[arg(short, long)]
+    download_folder: PathBuf,
+}
 
 fn main() {
     let mut log_builder = env_logger::builder();
     log_builder.filter_level(log::LevelFilter::Error).init();
-
     let builder = PrometheusBuilder::new();
     builder.install().unwrap();
-    // TODO Should start dht first
-    let torrent_info =
-        lava_torrent::torrent::v1::Torrent::read_from_file("linux-mint.torrent").unwrap();
+
+    let cli = Cli::parse();
+
     let (tx, rc) = std::sync::mpsc::channel();
 
-    let info_hash = torrent_info.info_hash_bytes();
-    let info_hash = Id::from_bytes(&info_hash).unwrap();
+    let state = match cli.torrent_info {
+        TorrentInfo {
+            info_hash: Some(info_hash),
+            torrent_file: None,
+        } => State::unstarted(decode_info_hash_hex(&info_hash), cli.download_folder),
+        TorrentInfo {
+            info_hash: None,
+            torrent_file: Some(metadata),
+        } => {
+            let parsed_metadata =
+                lava_torrent::torrent::v1::Torrent::read_from_file(metadata).unwrap();
+            State::unstarted_from_metadata(parsed_metadata, cli.download_folder).unwrap()
+        }
+        _ => unreachable!(),
+    };
+
+    let info_hash_id = Id::from_bytes(state.info_hash()).unwrap();
 
     std::thread::spawn(move || {
         let mut builder = Dht::builder();
@@ -34,11 +84,11 @@ fn main() {
         log::info!("Bootstrapping!");
         dht_client.bootstrapped();
         log::info!("Bootstrapping done!");
-        dht_client.announce_peer(info_hash, None).unwrap();
+        dht_client.announce_peer(info_hash_id, None).unwrap();
 
         loop {
             // query
-            let all_peers = dht_client.get_peers(info_hash);
+            let all_peers = dht_client.get_peers(info_hash_id);
             for peers in all_peers {
                 log::info!("Got {} peers", peers.len());
                 tx.send(Command::ConnectToPeers(peers)).unwrap();
@@ -51,8 +101,8 @@ fn main() {
     });
 
     let id = generate_peer_id();
-    let torrrent = Torrent::new(torrent_info, id);
+    let mut torrrent = Torrent::new(id, state);
     let start_time = Instant::now();
-    torrrent.start(rc, "downloaded").unwrap();
+    torrrent.start(rc).unwrap();
     println!("DOWNLOADED in {}", start_time.elapsed().as_secs());
 }
